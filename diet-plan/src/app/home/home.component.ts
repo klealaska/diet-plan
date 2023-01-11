9e6b493d1f8e920958590036b092b62b4fd67a58
klealaska <laskaklea@gmail.com>
2023-01-11 11:11:20 +0100

    primeng installed

--- diet-plan/src/app/home/home.component.ts
+++ diet-plan/src/app/home/home.component.ts
@@ -1,9 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import {ButtonModule} from 'primeng/button';
+
+
 
 @Component({
   standalone:true,
-  imports: [CommonModule],
+  imports: [CommonModule,ButtonModule],
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
