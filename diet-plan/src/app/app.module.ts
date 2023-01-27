9fc8e2f4119e190c59103e2690f2e4e17bf34916
k.laska <klea.laska@moveengineering-eu.com>
2023-01-27 14:29:58 +0100

    installing nebular theme

--- diet-plan/src/app/app.module.ts
+++ diet-plan/src/app/app.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NbLayoutModule, NbThemeModule } from '@nebular/theme';
+
 
 @NgModule({
   declarations: [
@@ -9,7 +12,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    NbThemeModule.forRoot(),
+    NbLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- diet-plan/src/app/home/home.component.ts
+++ diet-plan/src/app/home/home.component.ts
@@ -1,21 +1,30 @@
+import { Component, OnInit } from "@angular/core";
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
-import {ButtonModule} from 'primeng/button';
+import { FormsModule } from "@angular/forms";
+import { NbLayoutModule, NbSelectModule } from "@nebular/theme";
 
 
 
 @Component({
-  standalone:true,
-  imports: [CommonModule,ButtonModule],
+  standalone: true,
+  imports: [
+    CommonModule,
+    FormsModule,
+    NbLayoutModule,
+    NbSelectModule,
+  ],
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
 
+  selectedItem='';
+
   constructor() { }
 
   ngOnInit(): void {
+ 
   }
 
 }
